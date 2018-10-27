26f1ecd5c97ab6fc73dd3f46dd73e87ef3f284ae
Logan C. Brooks <lcbrooks@andrew.cmu.edu>
2018-10-26 20:37:59 -0400

    Update create_norovirus_sensors_table.sql to include `target`

--- src/util/create_norovirus_sensors_table.py
+++ src/util/create_norovirus_sensors_table.py
@@ -1,15 +1,12 @@
 CREATE TABLE `norovirus_sensors`(
-  `id`            INT(11)     NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  -- `target_source` VARCHAR(8)  NOT NULL                           ,
-  -- `target_column` VARCHAR(8)  NOT NULL                           ,
-  `name`          VARCHAR(8)  NOT NULL                           ,
-  `epiweek`       INT(11)     NOT NULL                           ,
-  `location`      VARCHAR(12) NULL                               ,
-  `value`         FLOAT       NOT NULL                           ,
-  -- UNIQUE KEY `entry` (`target_source`, `target_column`, `name`, `epiweek`, `location`),
-  UNIQUE KEY `entry` (`name`, `epiweek`, `location`),
-  -- KEY `sensor` (`target_source`, `target_column`, `name`),
-  KEY `name` (`name`),
+  `id`            INT(11)      NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `target`        VARCHAR(32)  NOT NULL                           ,
+  `name`          VARCHAR(8)   NOT NULL                           ,
+  `epiweek`       INT(11)      NOT NULL                           ,
+  `location`      VARCHAR(12)  NULL                               ,
+  `value`         FLOAT        NOT NULL                           ,
+  UNIQUE KEY `entry` (`target`, `name`, `epiweek`, `location`),
+  KEY `sensor` (`target`, `name`),
   KEY `epiweek` (`epiweek`),
   KEY `location` (`location`)
 );
